f907e0cd2bc1e2f777e70f58cf2159b1b97f0c7c
Angga Ari Wijaya <anggadarkprince@gmail.com>
2019-07-29 21:03:35 +0700

    add selection reducer

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, LayoutAnimation } from 'react-native';
+import { connect } from 'react-redux';
 import CardSection from './card/CardSection';
+import * as actions from '../actions';
 
 class ListItem extends Component {
+
+    componentWillUpdate() {
+        LayoutAnimation.easeInEaseOut();
+    }
+
+    renderDescription() {
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={{ flex: 1 }}>{library.description}</Text>
+                </CardSection>
+            );
+        }
+    }
+
     render() {
+        const { id, title } = this.props.library;
+
         return (
-            <CardSection key={this.props.library.id}>
-                <Text style={styles.title}>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
+                <View>
+                    <CardSection key={this.props.library.id}>
+                        <Text style={styles.title}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
 
 const styles = {
     title: {
-        fontSize: 19,
+        fontSize: 18,
         paddingLeft: 10
     }
 };
 
-export default ListItem;
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
--- src/components/Redux.js
+++ src/components/Redux.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../reducers';
